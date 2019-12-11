96885a54af670e9e2ef02ded6d9fada68e613013
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2019-12-10 23:06:49 -0500

    updated js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,12 @@
 module.exports = client => {
     console.log(`Logged in as ${client.user.tag}!`);
+
+    client.user.setActivity("Coding", {type: "PLAYING"});
+
+    let allUsers = client.users.array();
+    for (let i = 0; i < allUsers.length; i++) {
+        if (!client.db.get(allUsers[i].id)) {
+            client.db.set(allUsers[i].id, {gold: 0, items: [], played: 0, win: 0, loss: 0, word: "", players: [], words: [], turn: 0, ingame: 0});
+        }
+    }
 };
