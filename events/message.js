8659538cc35a57eea2e9871a21b21b380a18c8f8
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2019-12-10 23:03:41 -0500

    updated message.js

--- events/message.js
+++ events/message.js
@@ -1,21 +1,14 @@
 const { prefix } = require('../config');
 
 module.exports = async (client, message) => {
-    let settings;
-    try {
-        settings = await client.getProfile(message.profile);
-    } catch (error) {
-        console.error(error);
-    }
-
     if (message.author.bot) return;
-    if (message.content.indexOf(prefix) !== 0) return;
+    if (message.content.indexOf(client.config.prefix) !== 0) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     const cmd = client.commands.get(command);
     if (!cmd) return;
 
-    cmd.run(client, message, args, settings);
+    cmd.run(client, message, args);
 };
